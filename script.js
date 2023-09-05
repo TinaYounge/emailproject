10901d2d753c056c3e8a0e571367d7957baf8837
Nga Nguyen <nganguyen@Ngas-Air.lan>
2023-09-05 20:39:24 +0900

    project init

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const languageReplies = {
   vn: {
     partialAddressInput:
-      "<p><strong>-Địa chỉ quý khách cung cấp bị thiếu một phần</strong></p>",
+      "<p><strong>-Địa chỉ quý khách cung cấp bị thiếu một phần, Thẻ rút tiền của quý khách sẽ được giao tới địa chỉ này.</strong></p>",
     lessThanSixMonthsResidency:
       "<p><strong>-Thẻ cư trú của quý khách còn hạn dưới 6 tháng. </strong></p>",
     partialRomajiNameInput:
-      "<p><strong>-Tên của quý khách bị thiếu một phần</strong></p>",
+      "<p><strong>-Tên của quý khách bị thiếu một phần, tên quý khách phải giống trên thẻ cư trú.</strong></p>",
     repeatedNameInput:
-      "<p><strong>-Tên của quý khách bị viết lặp lại</strong></p>",
+      "<p><strong>-Tên của quý khách bị viết lặp lại, tên của quý khách phải giống trên thẻ cư trú.</strong></p>",
     unableToVerifyThicknessOfID:
-      "<p><strong>-Không xác thực thẻ do ảnh chụp nghiêng không rõ dộ dày.</strong></p>",
+      "<p><strong>-Không xác thực thẻ cư trú do ảnh chụp nghiêng không rõ dộ dày.</strong></p>",
     foreignerUsedMyNumberCardToApply:
-      "<p><strong>-Quý khách là người nước ngoài nhưng dùng thẻ My Number để tạo tài khoản. Người nước ngoài sẽ dùng Thẻ cư trú.</strong></p>",
-    unclearImageOfID: "<p><strong>-Hình ảnh thẻ không rõ ràng</strong></p>",
+      "<p><strong>-Quý khách là người nước ngoài nhưng dùng thẻ My Number để tạo tài khoản. Quý khách vui lòng dùng Thẻ cư trú.</strong></p>",
+    unclearImageOfID:
+      "<p><strong>-Hình ảnh thẻ không rõ ràng, Quý khách vui lòng chụp lại rõ hơn.</strong></p>",
     somethingCoveringOfficialStampOnID:
-      "<p><strong>-Một phần thẻ (con dấu) bị che bởi ngón tay</strong></p>",
+      "<p><strong>-Một phần thẻ (con dấu) bị che bởi ngón tay, Quý khách vui lòng chụp lại rõ hơn.</strong></p>",
     partialAddressMismatch:
-      "<p><strong>-Một phần của địa chỉ quý khách cung cấp không giống trên thẻ cư trú</strong></p>",
+      "<p><strong>-Một phần của địa chỉ quý khách cung cấp không giống trên thẻ cư trú. Thẻ rút tiền của quý khách sẽ được giao tới địa chỉ này.</strong></p>",
     addressInputInRomaji:
       "<p><strong>-Địa chỉ nhập là Tiếng Anh. Quý khách cần nhập địa chỉ y hệt trên thẻ cư trú là Tiếng Nhật</strong></p>",
     partialRepeatedNameInput:
-      "<p><strong>-Một phần trong tên quý khách bị nhập lại nhiều lần</strong></p>",
+      "<p><strong>-Một phần trong tên quý khách bị nhập lại nhiều lần, tên của quý khách phải giống trên thẻ cư trú.</strong></p>",
     partialBuildingNameMismatch:
       "<p><strong>-Tên toàn nhà không giống chính xác như trong thẻ cư trú</strong></p>",
     birthdayMismatch:
@@ -28,11 +29,11 @@ const languageReplies = {
     partialKatakanaNameInput:
       "<p><strong>-Tên Katakana của quý khách bị thiếu một phần. Tên này sẽ được viết trên thẻ Rút tiền của quý khách. Quý khách có thể tìm thấy tên Katakana của mình trên thẻ bảo hiểm</strong></p>",
     roomNumberMismatch:
-      "<p><strong>-Số phòng không giống như trên thẻ cư trú</strong></p>",
+      "<p><strong>-Số phòng không giống như trên thẻ cư trú, Thẻ rút tiền của quý khách sẽ được giao tới địa chỉ này.</strong></p>",
     addressMismatch:
-      "<p><strong>-Địa chỉ quý khách cung cấp không giống trên thẻ cư trú</strong></p>",
+      "<p><strong>-Địa chỉ quý khách cung cấp không giống trên thẻ cư trú, Thẻ rút tiền của quý khách sẽ được giao tới địa chỉ này.</strong></p>",
     eyesClosedInSelfie:
-      "<p><strong>-Quý khách nhắm mắt khi chụp nên cần chụp lại ảnh.</strong></p>",
+      "<p><strong>-Quý khách nhắm mắt khi chụp nên cần chụp lại ảnh. Quý khách vui lòng chụp lại ảnh mở mắt rõ nét hơn.</strong></p>",
     rejectedByJintech: "<p><strong>-</strong></p>",
     missingCompanyName:
       "<p><strong>-Quý khách điền thiếu địa tên công ty. Quý khách có thể tìm thấy Tên công ty qua thẻ bảo hiểm hoặc phiếu lương.</strong></p>",
@@ -40,39 +41,49 @@ const languageReplies = {
             <p>
             </p>
         `,
-        howDoIChangeTheGIGAAppLanguage: "<p>-Thay đổi ngôn ngữ: <a href='https://intercom.help/gigabank/en/articles/6740402' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Hướng dẫn thay đổi ngôn ngữ của app sang tiếng Việt]</a></p>",
-        howDoTakeFacePicture: "<p>-Chụp ảnh khuôn mặt <a href='https://intercom.help/gigabank/vi/articles/6714154' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Hướng dẫn chụp ảnh khuôn mặt]</a></p>",
-        takingPhotosForTheIdentityVerificationProcess: "<p>-Cập nhật lại ảnh chụp để xác minh danh tính <a href='https://intercom.help/gigabank/vi/articles/7002469' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Cách chụp ảnh thẻ ]</a></p>",
-        inputDddressInTheGIGAAp: "<p>-Địa chỉ tại cung cấp trên ứng dụng phải giống hệt như địa chỉ trên thẻ cư trú. <a href='https://intercom.help/gigabank/vi/articles/6796052' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Cách điền địa chỉ tại Nhật bản vào GIG-A app.]</a></p>"
-      },
-      en: {
-        residentCard6MonthsExpired: "<h2>GIG-A Update - Residency Card Expires in Less Than 6 Months</h2><p><strong>Thank you</strong> for reaching out to us. We have received your inquiry and will get back to you within 24 hours.</p><p>Best Regards,</p><p>Customer Service Team</p>"
-      }
-    };
-
+    howDoIChangeTheGIGAAppLanguage:
+      "<p>-Thay đổi ngôn ngữ: <a href='https://intercom.help/gigabank/vi/articles/6740402' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Hướng dẫn thay đổi ngôn ngữ của app sang tiếng Việt]</a></p>",
+    howDoTakeFacePicture:
+      "<p>-Chụp ảnh khuôn mặt <a href='https://intercom.help/gigabank/vi/articles/6714154' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Hướng dẫn chụp ảnh khuôn mặt]</a></p>",
+    takingPhotosForTheIdentityVerificationProcess:
+      "<p>-Cập nhật lại ảnh chụp để xác minh danh tính <a href='https://intercom.help/gigabank/vi/articles/7002469' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Cách chụp ảnh thẻ ]</a></p>",
+    inputDddressInTheGIGAAp:
+      "<p>-Địa chỉ tại cung cấp trên ứng dụng phải giống hệt như địa chỉ trên thẻ cư trú. <a href='https://intercom.help/gigabank/vi/articles/6796052' style='color: #007bff; text-decoration: none;' target='_blank' rel='noopener noreferrer'>[Cách điền địa chỉ tại Nhật bản vào GIG-A app.]</a></p>",
+  },
+  en: {
+    residentCard6MonthsExpired:
+      "<h2>GIG-A Update - Residency Card Expires in Less Than 6 Months</h2><p><strong>Thank you</strong> for reaching out to us. We have received your inquiry and will get back to you within 24 hours.</p><p>Best Regards,</p><p>Customer Service Team</p>",
+  },
+};
 
 function generateReply() {
   const customerNote1 = document.getElementById("customerNote1").value;
   const issueType = document.getElementById("issueType").value;
   const issueType2 = document.getElementById("issueType2").value;
-  
+
   const issueType3 = document.getElementById("issueType3").value;
   const issueType4 = document.getElementById("issueType4").value;
   const faq1 = document.getElementById("faq1").value;
+  const showFAQ = document.getElementById("faq1").value;
 
   const language = document.getElementById("language").value;
   let replyText = "";
   let replyText2 = "";
+  let replyText3 = "";
+  let replyText4 = "";
 
-  // if (!customerNote) {
-  //     replyText = "Please enter a customer name.";
-  // } else {
+  if (showFAQ === "no") {
+    replyshowFAQ = "";
+  } else {
+    replyshowFAQ =
+      "*Quý khách có thể tham khảo cách nộp lại đơn qua bài viết dưới đây";
+  }
+
   const template = languageReplies[language][issueType];
   const template2 = languageReplies[language][issueType2];
   const template3 = languageReplies[language][issueType3];
   const template4 = languageReplies[language][issueType4];
   const templatefaq1 = languageReplies[language][faq1];
-
 
   replyText = template.replace("{name}", customerNote1);
   replyText2 = template2;
@@ -80,14 +91,12 @@ function generateReply() {
   replyText4 = template4;
   replyTextFAQ1 = templatefaq1;
 
-
-
-  // }
+  document.getElementById("replyshowFAQ").innerHTML = replyshowFAQ; // Changed to innerHTML to support HTML tags
 
   document.getElementById("replyText").innerHTML = replyText; // Changed to innerHTML to support HTML tags
   document.getElementById("replyText2").innerHTML = replyText2; // Changed to innerHTML to support HTML tags
   document.getElementById("replyText3").innerHTML = replyText3; // Changed to innerHTML to support HTML tags
   document.getElementById("replyText4").innerHTML = replyText4; // Changed to innerHTML to support HTML tags
   document.getElementById("replyTextFAQ1").innerHTML = replyTextFAQ1; // Changed to innerHTML to support HTML tags
-
+  document.getElementById("replyTextFAQ1").innerHTML = replyTextFAQ1; // Changed to innerHTML to support HTML tags
 }
